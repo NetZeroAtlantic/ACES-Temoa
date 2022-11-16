38350f845cb1819c0417a9acdd4f6721bbb2cace
Cameron Wade <cameron@sutubra.ca>
2022-11-16 09:38:37 -0400

    Remove Inter-day ramping constraints
    Removed the ramping constraint linking the final hour
    of one day and the beginning hour of the subsequent day.
    
    This is because most users will be using representative days.
    In such a case, it is not fair to assume representative days
    should be linked via ramping limits.

--- temoa_model/temoa_model.py
+++ temoa_model/temoa_model.py
@@ -449,13 +449,18 @@ def temoa_create_model(name="Temoa"):
         M.RampConstraintDay_rpsdtv, rule=RampDownDay_Constraint
     )
 
-    M.RampConstraintSeason_rpstv = Set(dimen=5, initialize=RampConstraintSeasonIndices)
-    M.RampUpConstraintSeason = Constraint(
-        M.RampConstraintSeason_rpstv, rule=RampUpSeason_Constraint
-    )
-    M.RampDownConstraintSeason = Constraint(
-        M.RampConstraintSeason_rpstv, rule=RampDownSeason_Constraint
-    )
+
+    # Removing the RampUpSeason and RampDownSeason constraints.
+    # When using representative days, it is not fair to assume representative
+    # days are consecutive.
+    
+    # M.RampConstraintSeason_rpstv = Set(dimen=5, initialize=RampConstraintSeasonIndices)
+    # M.RampUpConstraintSeason = Constraint(
+        # M.RampConstraintSeason_rpstv, rule=RampUpSeason_Constraint
+    # )
+    # M.RampDownConstraintSeason = Constraint(
+        # M.RampConstraintSeason_rpstv, rule=RampDownSeason_Constraint
+    # )
 
     M.RampConstraintPeriod_rptv = Set(dimen=4, initialize=RampConstraintPeriodIndices)
     M.RampUpConstraintPeriod = Constraint(
